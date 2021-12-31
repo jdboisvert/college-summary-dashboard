f6c0c2cdaa19ec54afcefa9775d54eb4892ec818
Jeffrey Boisvert <jboisvert377@gmail.com>
2021-12-30 22:56:06 -0500

    Added basic Background Scheduler to run every 12 hours to scrap the Dawson College website to get and store the details.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit
 
 from exceptions.dashboard import NoCollegeMetricsFoundError
+from utils import DawsonCollegeWebsiteScrapper
 from utils.datastore import CollegeMetricsDataStore
 
 import logging
@@ -15,6 +18,19 @@ app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/collegeDashboardDB"
 
 mongodb_client = PyMongo(app)
 CollegeMetricsDataStore.db = mongodb_client.db
+
+
+def scrap_website():
+    logger.info("About to scrap website for college details.")
+    DawsonCollegeWebsiteScrapper().scrap()
+    logger.info("College scrapping completed with no errors.")
+
+
+background_scheduler = BackgroundScheduler(daemon=True)
+background_scheduler.add_job(scrap_website, "interval", hours=12)
+
+background_scheduler.start()
+atexit.register(lambda: background_scheduler.shutdown())
 
 
 @app.route("/about", methods=["GET"])
